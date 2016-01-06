abe3618e9fb4e6f99b22b0217c2d4f58ac0c3c43
worron <worrongm@gmail.com>
2016-01-07 02:48:55 +0300

    Filters: 'Dark and Glow' improved to custom version

--- scripts/filters/darkandglow/filter.py
+++ scripts/filters/darkandglow/filter.py
@@ -1,9 +1,34 @@
 import os
-from common import SimpleFilterBase
+from common import FilterParameter, CustomFilterBase
+from gi.repository import Gdk
 
-class Filter(SimpleFilterBase):
+class Filter(CustomFilterBase):
 
-	def __init__(self, *args):
-		SimpleFilterBase.__init__(self, os.path.dirname(__file__))
+	def __init__(self):
+		CustomFilterBase.__init__(self, os.path.dirname(__file__))
 		self.name = "Dark and Glow"
 		self.group = "Shadow"
+
+		gui_elements = ("window", "deviation", "scale")
+		self.gui_load(gui_elements)
+
+		visible_tag = self.dull['visual'].find(".//*[@id='visible1']")
+		blur_tag = self.dull['filter'].find(".//*[@id='feGaussianBlur1']")
+		self.param['deviation'] = FilterParameter(blur_tag, 'stdDeviation', '(.+)', '%.1f')
+		self.param['scale'] = FilterParameter(visible_tag, 'transform', 'scale\((.+?)\) ', 'scale(%.2f) ')
+
+		self.gui_setup()
+
+	def gui_setup(self):
+		self.gui['scale'].set_value(float(self.param['scale'].match()))
+		self.gui['deviation'].set_value(float(self.param['deviation'].match()))
+
+	def on_deviation_changed(self, scale):
+		value = scale.get_value()
+		self.param['deviation'].set_value(value)
+		self.render.run(False)
+
+	def on_scale_changed(self, scale):
+		value = scale.get_value()
+		self.param['scale'].set_value(value)
+		self.render.run(False)
