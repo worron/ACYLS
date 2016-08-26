5d72cb471ec44b43dc77f62097329a8c78cdb617
worron <worrongm@gmail.com>
2016-08-26 20:31:29 +0300

    Filters: clean 'Overlays' group

--- scripts/filters/tartan/filter.py
+++ scripts/filters/tartan/filter.py
@@ -16,22 +16,22 @@ class Filter(CustomFilterBase):
 		turbulence2_tag = self.dull['filter'].find(".//*[@id='feTurbulence2']")
 
 		self.param['scale'] = FilterParameter(visible_tag, 'transform', 'scale\((.+?)\) ', 'scale(%.2f) ')
-		self.param['octaves_x'] = FilterParameter(turbulence2_tag, 'numOctaves', '(.+)', '%d')
-		self.param['octaves_y'] = FilterParameter(turbulence1_tag, 'numOctaves', '(.+)', '%d')
+		self.param['octaves_x'] = FilterParameter(turbulence2_tag, 'numOctaves', '(.+)', '%.1f')
+		self.param['octaves_y'] = FilterParameter(turbulence1_tag, 'numOctaves', '(.+)', '%.1f')
 		self.param['frequency_x'] = FilterParameter(turbulence2_tag, 'baseFrequency', '(.+?) ', '%.2f ')
 		self.param['frequency_y'] = FilterParameter(turbulence1_tag, 'baseFrequency', ' (.+)', ' %.2f')
 
-		gui_elements = ("window", "scale", "octaves_x", "octaves_y", "frequency_x", "frequency_y")
+		gui_elements = ["scale", "octaves_x", "octaves_y", "frequency_x", "frequency_y"]
 
 		self.on_scale_changed = self.build_plain_handler('scale')
 		self.on_frequency_x_changed = self.build_plain_handler('frequency_x')
 		self.on_frequency_y_changed = self.build_plain_handler('frequency_y')
-		self.on_octaves_x_changed = self.build_plain_handler('octaves_x', translate=int)
-		self.on_octaves_y_changed = self.build_plain_handler('octaves_y', translate=int)
+		self.on_octaves_x_changed = self.build_plain_handler('octaves_x')
+		self.on_octaves_y_changed = self.build_plain_handler('octaves_y')
 
 		self.gui_load(gui_elements)
 		self.gui_setup()
 
 	def gui_setup(self):
-		self.gui_settler_plain('scale', 'frequency_x', 'frequency_y')
-		self.gui_settler_plain('octaves_x', 'octaves_x', translate=int)
+		self.gui_settler_plain('scale', 'frequency_x', 'frequency_y', 'octaves_x', 'octaves_y')
+		self.connect_scale_signal('scale', 'frequency_x', 'frequency_y', 'octaves_x', 'octaves_y')
